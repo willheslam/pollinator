9384a47b0fc5ffc81de8a6de7a3bf49a2dfaef8c
lodenrogue <phyzix5761@gmail.com>
2013-09-03 01:09:14 -0400

    init fix

---
+++ Pollinator-android/gen/com/loden/rogue/pollinator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.loden.rogue.pollinator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Pollinator-desktop/src/com/loden/rogue/pollinator/Main.java
@@ -0,0 +1,16 @@
+package com.loden.rogue.pollinator;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Pollinator";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Pollinator(), cfg);
+	}
+}
